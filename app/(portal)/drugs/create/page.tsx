cef268bf47c2feb0e669171ee5cc75430bc4bf14
Dede1421 <mauricioandre91@gmail.com>
2023-09-04 17:34:56 -0500

    feat: add some inputs for CreateDrugs form

--- app/(portal)/drugs/create/page.tsx
+++ app/(portal)/drugs/create/page.tsx
@@ -1,12 +1,52 @@
 "use client";
 
-import { Typography } from "@mui/material";
+import { Grid, Stack, Typography } from "@mui/material";
+import { Formik, Form, Field } from "formik";
+import { TextField } from "formik-mui";
 
 const CreateDrugsPage = () => {
+  const saveDrugForm = () => {
+    console.log("Guardando");
+  };
+
   return (
-    <div>
-      <Typography variant="h4">Registrar medicamento</Typography>
-    </div>
+    <Grid container spacing={2}>
+      <Grid item>
+        <Formik
+          initialValues={{
+            name: "",
+            description: "",
+          }}
+          onSubmit={saveDrugForm}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <Typography variant="h4">Registrar medicamento</Typography>
+              <Grid>
+                <Stack spacing={2}>
+                  <Field
+                    component={TextField}
+                    name="name"
+                    label="Nombre"
+                    variant="outlined"
+                    required
+                  />
+                  <Field
+                    component={TextField}
+                    name="description"
+                    label="Descripción"
+                    multiline
+                    rows={4}
+                    variant="outlined"
+                    required
+                  />
+                </Stack>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
+      </Grid>
+    </Grid>
   );
 };
 
