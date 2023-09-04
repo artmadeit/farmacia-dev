55e8366d86c352ce46b510cacd050540a2b705cd
Diana Quintanilla <dquintanilla@cognitiva.la>
2023-09-04 15:22:31 -0500

    feat: init antecedents selection

--- app/patient-interview/page.tsx
+++ app/patient-interview/page.tsx
@@ -1,20 +1,39 @@
 "use client";
 
-import { FormControlLabel, Radio } from "@mui/material";
+import { FormControlLabel, Radio, Checkbox } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import { Field, Form, Formik } from "formik";
-import { RadioGroup, TextField } from "formik-mui";
+import { RadioGroup, TextField, Switch, ToggleButtonGroup } from "formik-mui";
 import { formatDate } from "../date";
+import ToggleButton from "@mui/material/ToggleButton";
+//import Checkbox from "@mui/material/Checkbox";
 
 export default function PatientInterview() {
   const code = "AJK-203";
   const age = 18; // TODO: computed from fecha de nacimiento
+  const antecedents: any[] = [
+    { label: "IMA", name: "ima" },
+    { label: "ACV", name: "acv" },
+    { label: "ICC", name: "icc" },
+    { label: "Diabetes", name: "diabetes" },
+    { label: "Enf. Renal", name: "renal" },
+    { label: "Obesidad", name: "obesidad" },
+    { label: "Enf. Hepática", name: "hepatica" },
+    { label: "Úlcera", name: "ulcera" },
+    { label: "Enf. tiroides", name: "tiroides" },
+  ];
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
   return (
     <Formik
       initialValues={{
         occupation: "",
         sex: "",
+        weight: 0,
+        size: 0,
+        imc: 0,
+        other: "",
+        antecedents: [],
       }}
       onSubmit={() => {
         // TODO:
@@ -39,6 +58,54 @@ export default function PatientInterview() {
               <FormControlLabel value="MALE" control={<Radio />} label="M" />
               <FormControlLabel value="FEMALE" control={<Radio />} label="F" />
             </Field>
+          </Grid>
+          <Grid xs={4} display="flex" alignItems="center">
+            Peso:
+            <Field name="weigth" component={TextField} variant="outlined" />
+          </Grid>
+          <Grid xs={4} display="flex" alignItems="center">
+            Talla:
+            <Field name="size" component={TextField} variant="outlined" />
+          </Grid>
+          <Grid xs={4} display="flex" alignItems="center">
+            IMC:
+            <Field name="imc" component={TextField} variant="outlined" />
+          </Grid>
+        </Grid>
+        <Grid container spacing={2}>
+          <Grid xs={12}>
+            <strong>2. Historia de salud</strong>
+          </Grid>
+          <Grid xs={8} container>
+            <Field
+              component={ToggleButtonGroup}
+              name="antecedents"
+              type="checkbox"
+            >
+              <Grid xs={12} container>
+                {antecedents.map((item, index) => (
+                  <Grid xs={4}>
+                    {/* <Field
+                    key={index}
+                    component={CheckboxWithLabel}
+                    type="checkbox"
+                    name={item.name}
+                    Label={{ label: item.label }}
+                  /> */}
+                    <FormControlLabel
+                      key={index}
+                      value={item.name}
+                      control={<Radio />}
+                      label={item.label}
+                    />
+                  </Grid>
+                ))}
+              </Grid>
+            </Field>
+          </Grid>
+          <Grid xs={4}>
+            Otros:
+            <Field name="other" component={TextField} variant="outlined" />
           </Grid>
         </Grid>
       </Form>
