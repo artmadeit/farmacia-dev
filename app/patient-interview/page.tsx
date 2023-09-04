bd8d9c08fbdeea4813c9752d9695e6d5baedd69c
artmadeit <this.arthur.email@gmail.com>
2023-09-04 15:46:42 -0500

    fix: checkbox

--- app/patient-interview/page.tsx
+++ app/patient-interview/page.tsx
@@ -3,25 +3,31 @@
 import { FormControlLabel, Radio } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import { Field, Form, Formik } from "formik";
-import { RadioGroup, TextField, ToggleButtonGroup } from "formik-mui";
+import {
+  CheckboxWithLabel,
+  RadioGroup,
+  TextField,
+  ToggleButtonGroup,
+} from "formik-mui";
 import { DatePicker } from "formik-mui-x-date-pickers";
 import { formatDate } from "../date";
-//import Checkbox from "@mui/material/Checkbox";
+
+const antecedents = [
+  { label: "IMA", name: "ima" },
+  { label: "ACV", name: "acv" },
+  { label: "ICC", name: "icc" },
+  { label: "Diabetes", name: "diabetes" },
+  { label: "Enf. Renal", name: "renal" },
+  { label: "Obesidad", name: "obesidad" },
+  { label: "Enf. Hepática", name: "hepatica" },
+  { label: "Úlcera", name: "ulcera" },
+  { label: "Enf. tiroides", name: "tiroides" },
+];
 
 export default function PatientInterview() {
   const code = "AJK-203";
   const age = 18; // TODO: computed from fecha de nacimiento
-  const antecedents: any[] = [
-    { label: "IMA", name: "ima" },
-    { label: "ACV", name: "acv" },
-    { label: "ICC", name: "icc" },
-    { label: "Diabetes", name: "diabetes" },
-    { label: "Enf. Renal", name: "renal" },
-    { label: "Obesidad", name: "obesidad" },
-    { label: "Enf. Hepática", name: "hepatica" },
-    { label: "Úlcera", name: "ulcera" },
-    { label: "Enf. tiroides", name: "tiroides" },
-  ];
+
   const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
   return (
@@ -39,79 +45,79 @@ export default function PatientInterview() {
         // TODO:
       }}
     >
-      <Form>
-        <Grid container spacing={2}>
-          <Grid xs={10}>
-            <strong>1. Datos personales</strong>
+      {({ values }) => (
+        <Form>
+          <Grid container spacing={2}>
+            <Grid xs={10}>
+              <strong>1. Datos personales</strong>
+            </Grid>
+            <Grid xs={2}>Fecha: {formatDate(new Date())}</Grid>
+            <Grid xs={12}>Código del paciente: {code}</Grid>
+            <Grid xs={5} display="flex" alignItems="center">
+              Ocupación:
+              <Field
+                name="occupation"
+                component={TextField}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid xs={3}>
+              Fecha Nac:
+              <Field component={DatePicker} label="Fecha" name="date" />
+            </Grid>
+            <Grid xs={2}>Edad: {age}</Grid>
+            <Grid xs={2}>
+              Sexo:
+              <Field component={RadioGroup} name="sex" row>
+                <FormControlLabel value="MALE" control={<Radio />} label="M" />
+                <FormControlLabel
+                  value="FEMALE"
+                  control={<Radio />}
+                  label="F"
+                />
+              </Field>
+            </Grid>
+            <Grid xs={4} display="flex" alignItems="center">
+              Peso:
+              <Field name="weigth" component={TextField} variant="outlined" />
+            </Grid>
+            <Grid xs={4} display="flex" alignItems="center">
+              Talla:
+              <Field name="size" component={TextField} variant="outlined" />
+            </Grid>
+            <Grid xs={4} display="flex" alignItems="center">
+              IMC:
+              <Field name="imc" component={TextField} variant="outlined" />
+            </Grid>
           </Grid>
-          <Grid xs={2}>Fecha: {formatDate(new Date())}</Grid>
-          <Grid xs={12}>Código del paciente: {code}</Grid>
-          <Grid xs={5} display="flex" alignItems="center">
-            Ocupación:
-            <Field name="occupation" component={TextField} variant="outlined" />
-          </Grid>
-          <Grid xs={3}>
-            Fecha Nac:
-            <Field component={DatePicker} label="Fecha" name="date" />
-          </Grid>
-          <Grid xs={2}>Edad: {age}</Grid>
-          <Grid xs={2}>
-            Sexo:
-            <Field component={RadioGroup} name="sex" row>
-              <FormControlLabel value="MALE" control={<Radio />} label="M" />
-              <FormControlLabel value="FEMALE" control={<Radio />} label="F" />
-            </Field>
-          </Grid>
-          <Grid xs={4} display="flex" alignItems="center">
-            Peso:
-            <Field name="weigth" component={TextField} variant="outlined" />
-          </Grid>
-          <Grid xs={4} display="flex" alignItems="center">
-            Talla:
-            <Field name="size" component={TextField} variant="outlined" />
-          </Grid>
-          <Grid xs={4} display="flex" alignItems="center">
-            IMC:
-            <Field name="imc" component={TextField} variant="outlined" />
-          </Grid>
-        </Grid>
-        <Grid container spacing={2}>
-          <Grid xs={12}>
-            <strong>2. Historia de salud</strong>
-          </Grid>
-          <Grid xs={8} container>
-            <Field
-              component={ToggleButtonGroup}
-              name="antecedents"
-              type="checkbox"
-            >
+          <Grid container spacing={2}>
+            <Grid xs={12}>
+              <strong>2. Historia de salud</strong>
+            </Grid>
+            <Grid xs={8} container>
               <Grid xs={12} container>
-                {antecedents.map((item, index) => (
-                  <Grid xs={4} key={index}>
-                    {/* <Field
-                    key={index}
-                    component={CheckboxWithLabel}
-                    type="checkbox"
-                    name={item.name}
-                    Label={{ label: item.label }}
-                  /> */}
-                    <FormControlLabel
-                      key={index}
+                {antecedents.map((item) => (
+                  <Grid xs={4} key={item.name}>
+                    <Field
+                      component={CheckboxWithLabel}
+                      type="checkbox"
+                      name="antecedents"
                       value={item.name}
-                      control={<Radio />}
-                      label={item.label}
+                      Label={{ label: item.label }}
                     />
                   </Grid>
                 ))}
               </Grid>
-            </Field>
+            </Grid>
+            <Grid xs={4}>
+              Otros:
+              <Field name="other" component={TextField} variant="outlined" />
+            </Grid>
           </Grid>
-          <Grid xs={4}>
-            Otros:
-            <Field name="other" component={TextField} variant="outlined" />
-          </Grid>
-        </Grid>
-      </Form>
+
+          <div>{JSON.stringify(values)}</div>
+        </Form>
+      )}
     </Formik>
   );
 }
