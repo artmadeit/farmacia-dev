4c111122aed232ce8e736d7372195b7833767982
artmadeit <this.arthur.email@gmail.com>
2023-09-06 14:12:00 -0500

    feat: capture values from form patient selection

--- app/patient-selection/page.tsx
+++ app/patient-selection/page.tsx
@@ -1,15 +1,6 @@
 "use client";
 
-import {
-  Box,
-  Button,
-  Checkbox,
-  FormControl,
-  InputLabel,
-  ListSubheader,
-  MenuItem,
-  Select,
-} from "@mui/material";
+import { Box, Button, ListSubheader, MenuItem } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -17,37 +8,37 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import * as React from "react";
-import { PRM_GROUPS } from "./prm-groups";
-import { Form, Formik } from "formik";
-import { AsyncAutocomplete } from "../(components)/autocomplete";
+import { Field, Form, Formik } from "formik";
+import { Checkbox, Select } from "formik-mui";
+import { useRouter } from "next/navigation";
 import { api } from "../(api)/api";
 import { Page } from "../(api)/pagination";
-import { useRouter } from "next/navigation";
+import { AsyncAutocomplete } from "../(components)/autocomplete";
 import { Drug } from "../(portal)/drugs/Drug";
-import {
-  patientSelectionCriteriaList,
-  Criterio,
-} from "./patientSelectionCriteriaList";
+import { patientSelectionCriteriaList } from "./patientSelectionCriteriaList";
+import { PRM_GROUPS } from "./prm-groups";
 
 const PrmSelect = () => {
   return (
-    <FormControl sx={{ m: 1, width: "100%" }}>
-      <InputLabel htmlFor="grouped-select">PRM</InputLabel>
-      <Select defaultValue="" id="grouped-select" label="Grouping">
-        <MenuItem value="">
-          <em>Ninguno</em>
-        </MenuItem>
-        {PRM_GROUPS.map((x) => [
-          <ListSubheader key={x.group}>{x.group}</ListSubheader>,
-          ...x.items.map((item) => (
-            <MenuItem key={item.name} value={item.name}>
-              {item.name}: {item.description}
-            </MenuItem>
-          )),
-        ])}
-      </Select>
-    </FormControl>
+    <Field
+      component={Select}
+      formControl={{ sx: { m: 1, width: "100%" } }}
+      id="prm"
+      name="prm"
+      label="PRM"
+    >
+      <MenuItem value="">
+        <em>Ninguno</em>
+      </MenuItem>
+      {PRM_GROUPS.map((x) => [
+        <ListSubheader key={x.group}>{x.group}</ListSubheader>,
+        ...x.items.map((item) => (
+          <MenuItem key={item.name} value={item.name}>
+            {item.name}: {item.description}
+          </MenuItem>
+        )),
+      ])}
+    </Field>
   );
 };
 
@@ -80,61 +71,69 @@ export default function PatientSelectionPage() {
       <h1>Criterios de selección de pacientes</h1>
       <Formik
         initialValues={{
+          criterionList: [],
           drug: null,
+          prm: "",
         }}
         onSubmit={() => {
           // TODO:
         }}
       >
-        <Form>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell></TableCell>
-                  <TableCell>N°</TableCell>
-                  <TableCell>Criterios</TableCell>
-                  <TableCell align="right">Puntaje</TableCell>
-                  <TableCell>Referencias de ayuda</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {patientSelectionCriteriaList.map((row) => (
-                  <TableRow
-                    key={row.id}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                  >
-                    <TableCell>
-                      <Checkbox
-                        inputProps={{ "aria-label": "Checkbox demo" }}
-                      />
-                    </TableCell>
-                    <TableCell component="th" scope="row">
-                      {row.id}
-                    </TableCell>
-                    <TableCell>{row.name}</TableCell>
-                    <TableCell align="right">{row.score}</TableCell>
-                    <TableCell sx={{ width: "500px" }}>
-                      <HelpReference criteriaId={row.id} />
-                    </TableCell>
+        {({ values }) => (
+          <Form>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell></TableCell>
+                    <TableCell>N°</TableCell>
+                    <TableCell>Criterios</TableCell>
+                    <TableCell align="right">Puntaje</TableCell>
+                    <TableCell>Referencias de ayuda</TableCell>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <Box display="flex" justifyContent="end" paddingTop={2}>
-            <Button
-              onClick={() => {
-                // TODO: remove this when submit is done
-                console.log("redireccionando a patient interview");
-                router.push("patient-interview");
-              }}
-              variant="contained"
-            >
-              Continuar
-            </Button>
-          </Box>
-        </Form>
+                </TableHead>
+                <TableBody>
+                  {patientSelectionCriteriaList.map((row) => (
+                    <TableRow
+                      key={row.id}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell>
+                        <Field
+                          component={Checkbox}
+                          type="checkbox"
+                          name="criterionList"
+                          value={String(row.id)}
+                        />
+                      </TableCell>
+                      <TableCell component="th" scope="row">
+                        {row.id}
+                      </TableCell>
+                      <TableCell>{row.name}</TableCell>
+                      <TableCell align="right">{row.score}</TableCell>
+                      <TableCell sx={{ width: "500px" }}>
+                        <HelpReference criteriaId={row.id} />
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            {JSON.stringify(values)}
+            <Box display="flex" justifyContent="end" paddingTop={2}>
+              <Button
+                onClick={() => {
+                  // TODO: remove this when submit is done
+                  console.log("redireccionando a patient interview");
+                  router.push("patient-interview");
+                }}
+                variant="contained"
+              >
+                Continuar
+              </Button>
+            </Box>
+          </Form>
+        )}
       </Formik>
     </div>
   );
