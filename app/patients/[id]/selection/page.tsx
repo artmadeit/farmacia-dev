d791a90a69eee66c63e0bf3cccd5c0aed7a8c0f2
artmadeit <this.arthur.email@gmail.com>
2023-09-08 11:37:27 -0500

    feat: stepper linear

--- app/patients/[id]/selection/page.tsx
+++ app/patients/[id]/selection/page.tsx
@@ -1,189 +1,252 @@
 "use client";
 
-import {
-  Box,
-  Button,
-  ListSubheader,
-  MenuItem,
-  Typography,
-} from "@mui/material";
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import { Field, Form, Formik } from "formik";
-import { Checkbox, Select } from "formik-mui";
-import { useRouter } from "next/navigation";
-import { api } from "../../../(api)/api";
-import { Page } from "../../../(api)/pagination";
-import { AsyncAutocomplete } from "../../../(components)/autocomplete";
-import { Drug } from "../../../(portal)/drugs/Drug";
-import { patientSelectionCriteriaList } from "./patientSelectionCriteriaList";
-import { PRM_GROUPS } from "./prm-groups";
-import { sum } from "lodash";
-import yup from "../../../validation";
-import { Title } from "@/app/(components)/Title";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import React, { useState } from "react";
 
-const PrmSelect = () => {
-  return (
-    <Field
-      component={Select}
-      formControl={{ sx: { m: 1, width: "100%" } }}
-      id="prm"
-      name="prm"
-      label="PRM"
-    >
-      <MenuItem value="">
-        <em>Ninguno</em>
-      </MenuItem>
-      {PRM_GROUPS.map((x) => [
-        <ListSubheader key={x.group}>{x.group}</ListSubheader>,
-        ...x.items.map((item) => (
-          <MenuItem key={item.name} value={item.name}>
-            {item.name}: {item.description}
-          </MenuItem>
-        )),
-      ])}
-    </Field>
-  );
-};
+const steps = ["Selección", "Anamnesis", "Farmacoterapia"];
 
-const DrugAutocomplete = () => {
-  return (
-    <AsyncAutocomplete
-      label="Medicamento"
-      field="drug"
-      filter={(searchText) =>
-        api
-          .get<Page<Drug>>("drugs/search/findByNameContaining", {
-            params: { page: 0, searchText },
-          })
-          .then((x) => x.data._embedded.drugs)
-      }
-    />
-  );
-};
+export default function HorizontalLinearStepper() {
+  const [activeStep, setActiveStep] = useState(0);
 
-const CRITERIA_DRUG = 1;
-const CRITERIA_PRM = 8;
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
 
-const helpReferences = [
-  { criteriaId: CRITERIA_DRUG, component: DrugAutocomplete },
-  { criteriaId: CRITERIA_PRM, component: PrmSelect },
-];
-
-const getTotalScore = (criterionList: string[]) => {
-  return sum(
-    criterionList
-      .map((x) => Number(x))
-      .map((x) =>
-        patientSelectionCriteriaList.find((criterion) => criterion.id === x)
-      )
-      .map((criterion) => criterion?.score || 0)
-  );
-};
-
-export default function PatientSelectionPage() {
-  const router = useRouter();
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
 
   return (
-    <div>
-      <Title>Criterios de selección de pacientes</Title>
-      <Formik
-        initialValues={{
-          criterionList: [],
-          drug: null,
-          prm: "",
-        }}
-        validationSchema={yup.object({
-          drug: yup
-            .mixed()
-            .nullable()
-            .label("Medicamento")
-            .when("criterionList", {
-              is: (val: string[]) => val.includes(String(CRITERIA_DRUG)),
-              then: (schema) => schema.required(),
-              otherwise: (schema) => schema,
-            }),
-          prm: yup.string().when("criterionList", {
-            is: (val: string[]) => val.includes(String(CRITERIA_PRM)),
-            then: (schema) => schema.required(),
-            otherwise: (schema) => schema,
-          }),
+    <Box sx={{ width: "100%" }}>
+      <Stepper activeStep={activeStep}>
+        {steps.map((label) => {
+          return (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          );
         })}
-        onSubmit={() => {
-          // TODO: save data
-          router.push("/patients/1/interview");
-        }}
-      >
-        {({ values, errors }) => (
-          <Form>
-            <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell></TableCell>
-                    <TableCell>N°</TableCell>
-                    <TableCell>Criterios</TableCell>
-                    <TableCell align="right">Puntaje</TableCell>
-                    <TableCell>Referencias de ayuda</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {patientSelectionCriteriaList.map((row) => (
-                    <TableRow
-                      key={row.id}
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                    >
-                      <TableCell>
-                        <Field
-                          component={Checkbox}
-                          type="checkbox"
-                          name="criterionList"
-                          value={String(row.id)}
-                        />
-                      </TableCell>
-                      <TableCell component="th" scope="row">
-                        {row.id}
-                      </TableCell>
-                      <TableCell>{row.name}</TableCell>
-                      <TableCell align="right">{row.score}</TableCell>
-                      <TableCell sx={{ width: "500px" }}>
-                        <HelpReference criteriaId={row.id} />
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-            <Box display="flex" justifyContent="space-between" paddingTop={2}>
-              <Typography>
-                Puntaje total: {getTotalScore(values.criterionList)}
-                {getTotalScore(values.criterionList) >= 4 &&
-                  ", puntaje >= 4, considere SFT!"}
-              </Typography>
-              <Button variant="contained" type="submit">
-                Continuar
-              </Button>
-            </Box>
-          </Form>
-        )}
-      </Formik>
-    </div>
+      </Stepper>
+      {activeStep === steps.length ? (
+        <React.Fragment>
+          <Typography sx={{ mt: 2, mb: 1 }}>
+            Todos los procedimientos realizados
+          </Typography>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
+          <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+            <Button
+              color="inherit"
+              disabled={activeStep === 0}
+              onClick={handleBack}
+              sx={{ mr: 1 }}
+            >
+              Atrás
+            </Button>
+            <Box sx={{ flex: "1 1 auto" }} />
+            <Button onClick={handleNext}>
+              {activeStep === steps.length - 1 ? "Finalizar" : "Continuar"}
+            </Button>
+          </Box>
+        </React.Fragment>
+      )}
+    </Box>
   );
 }
 
-const HelpReference = ({ criteriaId }: { criteriaId: number }) => {
-  const data = helpReferences.find((x) => x.criteriaId == criteriaId);
-  if (!data) return "";
-  const Component = data.component;
+// "use client";
 
-  return (
-    <div>
-      <Component />
-    </div>
-  );
-};
+// import {
+//   Box,
+//   Button,
+//   ListSubheader,
+//   MenuItem,
+//   Typography,
+// } from "@mui/material";
+// import Paper from "@mui/material/Paper";
+// import Table from "@mui/material/Table";
+// import TableBody from "@mui/material/TableBody";
+// import TableCell from "@mui/material/TableCell";
+// import TableContainer from "@mui/material/TableContainer";
+// import TableHead from "@mui/material/TableHead";
+// import TableRow from "@mui/material/TableRow";
+// import { Field, Form, Formik } from "formik";
+// import { Checkbox, Select } from "formik-mui";
+// import { useRouter } from "next/navigation";
+// import { api } from "../../../(api)/api";
+// import { Page } from "../../../(api)/pagination";
+// import { AsyncAutocomplete } from "../../../(components)/autocomplete";
+// import { Drug } from "../../../(portal)/drugs/Drug";
+// import { patientSelectionCriteriaList } from "./patientSelectionCriteriaList";
+// import { PRM_GROUPS } from "./prm-groups";
+// import { sum } from "lodash";
+// import yup from "../../../validation";
+// import { Title } from "@/app/(components)/Title";
+
+// const PrmSelect = () => {
+//   return (
+//     <Field
+//       component={Select}
+//       formControl={{ sx: { m: 1, width: "100%" } }}
+//       id="prm"
+//       name="prm"
+//       label="PRM"
+//     >
+//       <MenuItem value="">
+//         <em>Ninguno</em>
+//       </MenuItem>
+//       {PRM_GROUPS.map((x) => [
+//         <ListSubheader key={x.group}>{x.group}</ListSubheader>,
+//         ...x.items.map((item) => (
+//           <MenuItem key={item.name} value={item.name}>
+//             {item.name}: {item.description}
+//           </MenuItem>
+//         )),
+//       ])}
+//     </Field>
+//   );
+// };
+
+// const DrugAutocomplete = () => {
+//   return (
+//     <AsyncAutocomplete
+//       label="Medicamento"
+//       field="drug"
+//       filter={(searchText) =>
+//         api
+//           .get<Page<Drug>>("drugs/search/findByNameContaining", {
+//             params: { page: 0, searchText },
+//           })
+//           .then((x) => x.data._embedded.drugs)
+//       }
+//     />
+//   );
+// };
+
+// const CRITERIA_DRUG = 1;
+// const CRITERIA_PRM = 8;
+
+// const helpReferences = [
+//   { criteriaId: CRITERIA_DRUG, component: DrugAutocomplete },
+//   { criteriaId: CRITERIA_PRM, component: PrmSelect },
+// ];
+
+// const getTotalScore = (criterionList: string[]) => {
+//   return sum(
+//     criterionList
+//       .map((x) => Number(x))
+//       .map((x) =>
+//         patientSelectionCriteriaList.find((criterion) => criterion.id === x)
+//       )
+//       .map((criterion) => criterion?.score || 0)
+//   );
+// };
+
+// export default function PatientSelectionPage() {
+//   const router = useRouter();
+
+//   return (
+//     <div>
+//       <Title>Criterios de selección de pacientes</Title>
+//       <Formik
+//         initialValues={{
+//           criterionList: [],
+//           drug: null,
+//           prm: "",
+//         }}
+//         validationSchema={yup.object({
+//           drug: yup
+//             .mixed()
+//             .nullable()
+//             .label("Medicamento")
+//             .when("criterionList", {
+//               is: (val: string[]) => val.includes(String(CRITERIA_DRUG)),
+//               then: (schema) => schema.required(),
+//               otherwise: (schema) => schema,
+//             }),
+//           prm: yup.string().when("criterionList", {
+//             is: (val: string[]) => val.includes(String(CRITERIA_PRM)),
+//             then: (schema) => schema.required(),
+//             otherwise: (schema) => schema,
+//           }),
+//         })}
+//         onSubmit={() => {
+//           // TODO: save data
+//           router.push("/patients/1/interview");
+//         }}
+//       >
+//         {({ values, errors }) => (
+//           <Form>
+//             <TableContainer component={Paper}>
+//               <Table sx={{ minWidth: 650 }} aria-label="simple table">
+//                 <TableHead>
+//                   <TableRow>
+//                     <TableCell></TableCell>
+//                     <TableCell>N°</TableCell>
+//                     <TableCell>Criterios</TableCell>
+//                     <TableCell align="right">Puntaje</TableCell>
+//                     <TableCell>Referencias de ayuda</TableCell>
+//                   </TableRow>
+//                 </TableHead>
+//                 <TableBody>
+//                   {patientSelectionCriteriaList.map((row) => (
+//                     <TableRow
+//                       key={row.id}
+//                       sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+//                     >
+//                       <TableCell>
+//                         <Field
+//                           component={Checkbox}
+//                           type="checkbox"
+//                           name="criterionList"
+//                           value={String(row.id)}
+//                         />
+//                       </TableCell>
+//                       <TableCell component="th" scope="row">
+//                         {row.id}
+//                       </TableCell>
+//                       <TableCell>{row.name}</TableCell>
+//                       <TableCell align="right">{row.score}</TableCell>
+//                       <TableCell sx={{ width: "500px" }}>
+//                         <HelpReference criteriaId={row.id} />
+//                       </TableCell>
+//                     </TableRow>
+//                   ))}
+//                 </TableBody>
+//               </Table>
+//             </TableContainer>
+//             <Box display="flex" justifyContent="space-between" paddingTop={2}>
+//               <Typography>
+//                 Puntaje total: {getTotalScore(values.criterionList)}
+//                 {getTotalScore(values.criterionList) >= 4 &&
+//                   ", puntaje >= 4, considere SFT!"}
+//               </Typography>
+//               <Button variant="contained" type="submit">
+//                 Continuar
+//               </Button>
+//             </Box>
+//           </Form>
+//         )}
+//       </Formik>
+//     </div>
+//   );
+// }
+
+// const HelpReference = ({ criteriaId }: { criteriaId: number }) => {
+//   const data = helpReferences.find((x) => x.criteriaId == criteriaId);
+//   if (!data) return "";
+//   const Component = data.component;
+
+//   return (
+//     <div>
+//       <Component />
+//     </div>
+//   );
+// };
