23ab22fd54d20c9368c070dba41b3ce4c8852959
artmadeit <this.arthur.email@gmail.com>
2023-09-10 12:25:54 -0500

    feat: get parent

--- app/patients/[id]/HorizontalStepper.tsx
+++ app/patients/[id]/HorizontalStepper.tsx
@@ -4,31 +4,42 @@ import Box from "@mui/material/Box";
 import Step from "@mui/material/Step";
 import StepButton from "@mui/material/StepButton";
 import Stepper from "@mui/material/Stepper";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 
-const steps = ["Selección", "Anamnesis", "Farmacoterapia"];
+type Step = { path: string; label: string };
 
-export function HorizontalStepper({
-  activeStep: initialActiveStep,
-}: {
-  activeStep: number;
-}) {
-  const [activeStep, setActiveStep] = React.useState(initialActiveStep);
+const steps = [
+  { path: "selection", label: "Selección" },
+  { path: "medical-history", label: "Anamnesis" },
+  { path: "todo", label: "Farmacoterapia" },
+];
+
+const getParent = (path: string) => {
+  const pathSplit = path.split("/");
+  pathSplit.pop();
+  return pathSplit.join("/");
+};
+
+export function HorizontalStepper({ activeStep }: { activeStep: number }) {
+  const router = useRouter();
+  const pathname = usePathname();
+
   const [completed] = React.useState<{
     [k: number]: boolean;
   }>({});
 
-  const handleStep = (step: number) => () => {
-    setActiveStep(step);
+  const handleStep = (step: Step) => () => {
+    router.push(getParent(pathname) + "/" + step.path);
   };
 
   return (
     <Box sx={{ width: "100%" }} py={4}>
       <Stepper nonLinear activeStep={activeStep}>
-        {steps.map((label, index) => (
-          <Step key={label} completed={completed[index]}>
-            <StepButton color="inherit" onClick={handleStep(index)}>
-              {label}
+        {steps.map((step, index) => (
+          <Step key={step.path} completed={completed[index]}>
+            <StepButton color="inherit" onClick={handleStep(step)}>
+              {step.label}
             </StepButton>
           </Step>
         ))}
