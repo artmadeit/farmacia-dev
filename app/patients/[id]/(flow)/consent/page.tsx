7300c26dacb65a441841b8cb1b592a7e3c0df3b8
artmadeit <this.arthur.email@gmail.com>
2023-09-12 15:14:57 -0500

    feat: add uppy

--- app/patients/[id]/(flow)/consent/page.tsx
+++ app/patients/[id]/(flow)/consent/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import { Title } from "@/app/(components)/Title";
+import Uppy from "@uppy/core";
+import { DragDrop } from "@uppy/react";
+
+import "@uppy/core/dist/style.min.css";
+import "@uppy/drag-drop/dist/style.min.css";
+
+const uppy = new Uppy();
 
 export default function ConsentPage() {
   return (
     <div>
       <Title>Firma de consentimiento</Title>
+      <DragDrop uppy={uppy} />
     </div>
   );
 }
