51352069cf903bcd09423c32a19c761603dc6401
Dede1421 <mauricioandre91@gmail.com>
2023-09-12 13:32:50 -0500

    feat: add useSWR for Edit patient

--- app/patients/[id]/page.tsx
+++ app/patients/[id]/page.tsx
@@ -1,9 +1,15 @@
-const EditPatient = () => {
-  return (
-    <div>
-      <h1>Edit Page</h1>
-    </div>
-  );
+"use client";
+
+import React from "react";
+import useSWR from "swr";
+import PatientForm from "../create/PatientForm";
+
+const EditPatient = ({ params }: { params: { id: number } }) => {
+  const { id } = params;
+  const { data: patient } = useSWR(id ? `patients/${id}` : null);
+
+  if(!patient) return <div>Loading</div>
+  return <PatientForm patient={patient} />;
 };
 
 export default EditPatient;
