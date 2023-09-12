68a15b57d678a3b088689a070babb755d8ed2672
artmadeit <this.arthur.email@gmail.com>
2023-09-12 18:33:31 -0500

    feat: on load show embed

--- app/patients/[id]/(flow)/consent/page.tsx
+++ app/patients/[id]/(flow)/consent/page.tsx
@@ -10,6 +10,7 @@ import "@uppy/drag-drop/dist/style.min.css";
 import Spanish from "@uppy/locales/lib/es_ES";
 import Transloadit from "@uppy/transloadit";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import { useState } from "react";
 
 const uppy = new Uppy({
   locale: Spanish,
@@ -23,9 +24,35 @@ const uppy = new Uppy({
       template_id: "99d5c10e242049c8a089a4306d30698c",
     },
   },
+  waitForEncoding: true,
 });
 
 export default function ConsentPage() {
+  const [uploadUrl, setUploadUrl] = useState<string | ArrayBuffer | null>("");
+
+  // uppy.on("upload-success", (file, response) => {
+  //   console.log(file);
+  //   console.log(response);
+
+  //   if (response.uploadURL) {
+  //     setUploadUrl(response.uploadURL);
+  //   }
+  // });
+
+  uppy.on("transloadit:result", (stepName, result: any) => {
+    console.log("lalala");
+    const file = uppy.getFile(result.localId);
+    console.log({ file });
+
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      if (e.target) {
+        setUploadUrl(e.target.result);
+      }
+    };
+    reader.readAsDataURL(file.data);
+  });
+
   return (
     <div>
       <Title>Firma de consentimiento</Title>
@@ -35,12 +62,14 @@ export default function ConsentPage() {
           <StatusBar uppy={uppy} />
         </Grid>
         <Grid xs={6}>
-          <embed
-            src="/lala.pdf"
-            width="500"
-            height="375"
-            type="application/pdf"
-          />
+          {uploadUrl && (
+            <embed
+              src={uploadUrl as any}
+              width="500"
+              height="375"
+              type="application/pdf"
+            />
+          )}
         </Grid>
       </Grid>
     </div>
