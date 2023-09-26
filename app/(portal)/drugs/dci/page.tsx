c0bf7a7790044c77eb1e938c8a959f3c51db3666
Dede1421 <mauricioandre91@gmail.com>
2023-09-26 16:20:09 -0500

    feat: add a DataGrid table

--- app/(portal)/drugs/dci/page.tsx
+++ app/(portal)/drugs/dci/page.tsx
@@ -1,13 +1,50 @@
+"use client";
+
+import React from "react";
 import { Stack, Typography } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { withOutSorting } from "@/app/(components)/helpers/withOutSorting";
+import { usePagination } from "@/app/(components)/hook-customization/usePagination";
+import { DrugDci } from "./DrugDci";
+import useSWR from "swr";
+import { Page } from "@/app/(api)/pagination";
 
 const DciList = () => {
+  const { paginationModel, setPaginationModel } = usePagination();
+
+  const { data: drugDcis } = useSWR<Page<DrugDci>>([
+    "/drugDcis",
+    { params: { page: paginationModel.page, size: paginationModel.pageSize } },
+  ]);
+
+  const columns = React.useMemo(
+    () =>
+      (
+        [
+          { field: "name", headerName: "Nombre", width: 200 },
+        ] as GridColDef<DrugDci>[]
+      ).map(withOutSorting),
+    []
+  );
+
+  if (!drugDcis) return <div>Loading</div>;
   return (
     <Stack spacing={2}>
       <Stack spacing={2}>
-        <Typography variant="h4">DCI</Typography>
+        <Typography variant="h4">
+          Denominación común internacional (DCI)
+        </Typography>
       </Stack>
       <div>
-        DCI tabla
+        <DataGrid
+          columns={columns}
+          rowCount={drugDcis.page.totalElements}
+          rows={drugDcis._embedded.drugDcis}
+          paginationModel={paginationModel}
+          paginationMode="server"
+          onPaginationModelChange={setPaginationModel}
+          // localeText={}
+        />
       </div>
     </Stack>
   );
