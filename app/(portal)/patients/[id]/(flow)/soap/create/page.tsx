43dc8a30a2ca31b8d6fc81683a51daf05f69f2fe
artmadeit <this.arthur.email@gmail.com>
2023-10-21 21:31:13 -0500

    feat: soap

--- app/(portal)/patients/[id]/(flow)/soap/create/page.tsx
+++ app/(portal)/patients/[id]/(flow)/soap/create/page.tsx
@@ -3,11 +3,12 @@
 import { Title } from "@/app/(components)/Title";
 import { Divider } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
-import { Form, Formik } from "formik";
+import { Field, Form, Formik } from "formik";
 import {
   PharmacotherapyTable,
   emptyHistoryRow,
 } from "../../pharmacotherapy/PharmacotherapyTable";
+import { TextField } from "formik-mui";
 
 export default function CreateSoap() {
   return (
@@ -17,17 +18,73 @@ export default function CreateSoap() {
       <Formik
         initialValues={{
           history: [{ ...emptyHistoryRow }],
+          subjective: "",
+          objective: "",
+          analysis: "",
+          plan: "",
         }}
         onSubmit={() => {}}
       >
         {({ values, errors }) => (
           <Form>
-            <Grid container spacing={2}>
-              <Grid xs={10} style={{ margin: "10px 0px" }}>
+            <Grid container spacing={4}>
+              <Grid xs={10} pt={4}>
                 <strong>Farmacoterapia (P) Prescrito (A) Automedicado </strong>
               </Grid>
+              <Grid xs={12}>
+                <PharmacotherapyTable name="history" values={values} />
+              </Grid>
+              <Grid xs={12} container spacing={1}>
+                <Grid xs={1} alignItems="center" display="flex">
+                  S
+                </Grid>
+                <Grid xs={11}>
+                  <Field
+                    component={TextField}
+                    name="subjective"
+                    label="Subjetivo"
+                    variant="outlined"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid xs={1} alignItems="center" display="flex">
+                  O
+                </Grid>
+                <Grid xs={11}>
+                  <Field
+                    component={TextField}
+                    name="objective"
+                    label="Objetivo"
+                    variant="outlined"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid xs={1} alignItems="center" display="flex">
+                  A
+                </Grid>
+                <Grid xs={11}>
+                  <Field
+                    component={TextField}
+                    name="analysis"
+                    label="Análisis"
+                    variant="outlined"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid xs={1} alignItems="center" display="flex">
+                  P
+                </Grid>
+                <Grid xs={11}>
+                  <Field
+                    component={TextField}
+                    name="plan"
+                    label="Plan"
+                    variant="outlined"
+                    fullWidth
+                  />
+                </Grid>
+              </Grid>
             </Grid>
-            <PharmacotherapyTable name="history" values={values} />
           </Form>
         )}
       </Formik>
