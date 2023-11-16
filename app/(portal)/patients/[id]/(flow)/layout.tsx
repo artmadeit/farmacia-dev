3a58397eb7715fc6f2f6d25fb17e8166f75a77a0
Dede1421 <mauricioandre91@gmail.com>
2023-11-16 12:33:43 -0500

    feat(layout): add an Edit IconButton

--- app/(portal)/patients/[id]/(flow)/layout.tsx
+++ app/(portal)/patients/[id]/(flow)/layout.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { Typography } from "@mui/material";
+import { Box, Button, IconButton, Tooltip, Typography } from "@mui/material";
 import { HorizontalStepper } from "./HorizontalStepper";
 import { apiUrl } from "@/app/(api)/api";
 import { Patient } from "../../create/Patient";
 import useSWR from "swr";
+import EditIcon from "@mui/icons-material/Edit";
+import { useRouter } from "next/navigation";
 
 export default function MedicalFlowLayout({
   params,
@@ -18,12 +20,27 @@ export default function MedicalFlowLayout({
     `${apiUrl}/patients/${id}`
   );
 
+  const router = useRouter();
+
   if (isLoading || !patient) return <div>Loading</div>;
   return (
     <div>
-      <Typography variant="h6" gutterBottom>
-        Paciente: {patient.firstName} {patient.lastName}, código: {patient.code}
-      </Typography>
+      <Box display="flex" alignItems="center">
+        <Typography variant="h6" gutterBottom>
+          Paciente: {patient.firstName} {patient.lastName}, código:{" "}
+          {patient.code}
+        </Typography>
+        <Tooltip title="Editar">
+          <IconButton
+            aria-label="edit"
+            sx={{ marginLeft: 1, marginBottom: "0.35em" }}
+            onClick={() => router.push(`/patients/${id}`)}
+          >
+            <EditIcon />
+          </IconButton>
+        </Tooltip>
+      </Box>
+
       <HorizontalStepper patientId={id} />
       {children}
     </div>
