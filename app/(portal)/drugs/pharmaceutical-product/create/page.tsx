55a1667d50033995a8bea6151b8aa79ee0b86080
Dede1421 <mauricioandre91@gmail.com>
2024-01-30 09:35:37 -0500

    feat: create a PharmaceuticalProductForm component

--- app/(portal)/drugs/pharmaceutical-product/create/page.tsx
+++ app/(portal)/drugs/pharmaceutical-product/create/page.tsx
@@ -4,60 +4,62 @@ import { Button, Stack, Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import { Field, Form, Formik } from "formik";
 import { TextField } from "formik-mui";
+import PharmaceuticalProductForm from "../pharmaceuticalProductForm";
 
 const CreatePharmaceuticalProduct = () => {
   return (
-    <Grid container spacing={2}>
-      <Grid>
-        <Formik
-          initialValues={{
-            name: "",
-            concentration: "",
-            shape: "",
-          }}
-          onSubmit={() => console.log("hola")}
-        >
-          {({ isSubmitting }) => (
-            <Form>
-              <Typography variant="h4">Productos Farmaceuticos</Typography>
-              <Grid>
-                <Stack spacing={2}>
-                  <Field
-                    component={TextField}
-                    name="name"
-                    label="Nombre"
-                    variant="outlined"
-                    required
-                  />
-                  <Field
-                    component={TextField}
-                    name="concentration"
-                    label="Concentración"
-                    variant="outlined"
-                    required
-                  />
-                  <Field
-                    component={TextField}
-                    name="shape"
-                    label="Forma"
-                    variant="outlined"
-                    required
-                  />
-                </Stack>
-                <Button
-                  type="submit"
-                  disabled={isSubmitting}
-                  variant="contained"
-                  sx={{ marginTop: "20px" }}
-                >
-                  Guardar
-                </Button>
-              </Grid>
-            </Form>
-          )}
-        </Formik>
-      </Grid>
-    </Grid>
+    <PharmaceuticalProductForm textName="Registrar" />
+    // <Grid container spacing={2}>
+    //   <Grid>
+    //     <Formik
+    //       initialValues={{
+    //         name: "",
+    //         concentration: "",
+    //         shape: "",
+    //       }}
+    //       onSubmit={() => console.log("hola")}
+    //     >
+    //       {({ isSubmitting }) => (
+    //         <Form>
+    //           <Typography variant="h4">Productos Farmaceuticos</Typography>
+    //           <Grid>
+    //             <Stack spacing={2}>
+    //               <Field
+    //                 component={TextField}
+    //                 name="name"
+    //                 label="Nombre"
+    //                 variant="outlined"
+    //                 required
+    //               />
+    //               <Field
+    //                 component={TextField}
+    //                 name="concentration"
+    //                 label="Concentración"
+    //                 variant="outlined"
+    //                 required
+    //               />
+    //               <Field
+    //                 component={TextField}
+    //                 name="shape"
+    //                 label="Forma"
+    //                 variant="outlined"
+    //                 required
+    //               />
+    //             </Stack>
+    //             <Button
+    //               type="submit"
+    //               disabled={isSubmitting}
+    //               variant="contained"
+    //               sx={{ marginTop: "20px" }}
+    //             >
+    //               Guardar
+    //             </Button>
+    //           </Grid>
+    //         </Form>
+    //       )}
+    //     </Formik>
+    //   </Grid>
+    // </Grid>
   );
 };
 
---
+++ app/(portal)/drugs/pharmaceutical-product/pharmaceuticalProductForm.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { Button, Stack, Typography } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import { Field, Form, Formik } from "formik";
+import { TextField } from "formik-mui";
+
+type PharmaceuticalProductFormProps = {
+  textName: string;
+};
+
+const PharmaceuticalProductForm = ({
+  textName,
+}: PharmaceuticalProductFormProps) => {
+  return (
+    <Grid container spacing={2}>
+      <Grid>
+        <Formik
+          initialValues={{
+            name: "",
+            concentration: "",
+            shape: "",
+          }}
+          onSubmit={() => console.log("hola")}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <Typography variant="h4">
+                {textName} productos farmaceuticos
+              </Typography>
+              <Grid>
+                <Stack spacing={2}>
+                  <Field
+                    component={TextField}
+                    name="name"
+                    label="Nombre"
+                    variant="outlined"
+                    required
+                  />
+                  <Field
+                    component={TextField}
+                    name="concentration"
+                    label="Concentración"
+                    variant="outlined"
+                    required
+                  />
+                  <Field
+                    component={TextField}
+                    name="shape"
+                    label="Forma"
+                    variant="outlined"
+                    required
+                  />
+                </Stack>
+                <Button
+                  type="submit"
+                  disabled={isSubmitting}
+                  variant="contained"
+                  sx={{ marginTop: "20px" }}
+                >
+                  Guardar
+                </Button>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default PharmaceuticalProductForm;
