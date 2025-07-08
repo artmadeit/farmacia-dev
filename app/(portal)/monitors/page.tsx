bdc99e62bb2ad8a418da66cae22549fcdc13e477
Dede1421 <mauricioandre91@gmail.com>
2025-07-08 12:22:19 -0500

    feat: add fab button

--- app/(portal)/monitors/page.tsx
+++ app/(portal)/monitors/page.tsx
@@ -1,10 +1,21 @@
-import { Stack, Typography } from "@mui/material";
+"use client";
+
+import AddIcon from "@mui/icons-material/Add";
+import { Fab, Stack, Tooltip, Typography } from "@mui/material";
+import Link from "next/link";
 
 export default function ListMonitors() {
   return (
     <Stack direction="column">
-      <Stack direction="row">
+      <Stack direction="row" alignItems="center">
         <Typography variant="h4">Monitores</Typography>
+        <Tooltip title="Registrar" style={{ paddingLeft: "10px" }}>
+          <Link href="/patients">
+            <Fab color="primary" aria-labelledby="add">
+              <AddIcon />
+            </Fab>
+          </Link>
+        </Tooltip>
       </Stack>
     </Stack>
   );
