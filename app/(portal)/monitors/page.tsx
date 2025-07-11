7342d74a645909dc15f415f44295d9660699bf49
Dede1421 <mauricioandre91@gmail.com>
2025-07-11 16:29:02 -0500

    feat: add a Checkbox in Monitor DataGrid table

--- app/(portal)/monitors/page.tsx
+++ app/(portal)/monitors/page.tsx
@@ -14,6 +14,7 @@ import {
   esES,
   GridActionsCellItem,
   GridColDef,
+  GRID_CHECKBOX_SELECTION_COL_DEF,
 } from "@mui/x-data-grid";
 import Link from "next/link";
 import React from "react";
@@ -76,6 +77,10 @@ export default function ListMonitors() {
               ];
             },
           },
+          {
+            ...GRID_CHECKBOX_SELECTION_COL_DEF,
+            width: 100,
+          },
         ] as GridColDef<Monitor>[]
       ).map(withOutSorting),
     [router]
@@ -118,6 +123,7 @@ export default function ListMonitors() {
           columns={columns}
           rows={monitor._embedded.monitor}
           localeText={esES.components.MuiDataGrid.defaultProps.localeText}
+          checkboxSelection
         />
       </div>
     </Stack>
