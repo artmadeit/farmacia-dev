47303f15b66a9d3b6759efbb449667becd53069b
artmadeit <this.arthur.email@gmail.com>
2025-07-12 17:59:30 -0500

    feat: show email monitor

--- app/(portal)/monitors/[email]/page.tsx
+++ app/(portal)/monitors/[email]/page.tsx
@@ -21,7 +21,9 @@ type Pharmacologist = {
   email: string;
 };
 
-const EditMonitors = () => {
+const EditMonitors = ({ params }: { params: { email: string } }) => {
+  const { email } = params;
+
   const router = useRouter();
   const [pharmacalogist, setPharmacalogist] = React.useState({
     _embedded: {
@@ -72,49 +74,13 @@ const EditMonitors = () => {
     [router]
   );
 
+  console.log(email)
   return (
     <Grid container style={{ display: "flex", flexDirection: "column" }}>
       <Grid item xs={6}>
-        <Formik
-          initialValues={{
-            name: "",
-            email: "",
-          }}
-          onSubmit={() => console.log("Enviando")}
-        >
-          {({ isSubmitting }) => (
-            <Form>
-              <Typography variant="h4" style={{ paddingBottom: "20px" }}>
-                Datos Generales del monitor
-              </Typography>
-              <Grid>
-                <Stack spacing={2}>
-                  <Field
-                    component={TextField}
-                    name="name"
-                    label="Nombre"
-                    variant="outlined"
-                  />
-                  <Field
-                    component={TextField}
-                    type="email"
-                    name="email"
-                    label="Email"
-                    variant="outlined"
-                  />
-                </Stack>
-                <Button
-                  type="submit"
-                  variant="contained"
-                  disabled={isSubmitting}
-                  style={{ marginTop: "20px" }}
-                >
-                  Guardar
-                </Button>
-              </Grid>
-            </Form>
-          )}
-        </Formik>
+        <Typography variant="h4" style={{ paddingBottom: "20px" }}>
+          Monitor: <span>{decodeURIComponent(email)}</span>
+        </Typography>
       </Grid>
       <div style={{ width: "100", height: "70vh" }}>
         <Typography variant="h5" style={{ padding: "40px 0px 20px 0px" }}>
@@ -126,7 +92,7 @@ const EditMonitors = () => {
           localeText={esES.components.MuiDataGrid.defaultProps.localeText}
           disableColumnFilter
           checkboxSelection
-          // disableRowSelectionOnClick
+        // disableRowSelectionOnClick
         />
         <Button variant="contained" style={{ margin: "20px 0px 20px 0px" }}>
           Guardar
