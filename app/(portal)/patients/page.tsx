392a2756ea1a6c1bab379bf8bb0cc08c0f3255d1
artmadeit <this.arthur.email@gmail.com>
2025-09-02 10:29:06 -0500

    fix: patients not showing good

--- app/(portal)/patients/page.tsx
+++ app/(portal)/patients/page.tsx
@@ -43,7 +43,7 @@ export default function ListPatients() {
     {
       params: {
         page: paginationModel.page,
-        pageSize: paginationModel.pageSize,
+        size: paginationModel.pageSize,
       },
     },
   ]);
